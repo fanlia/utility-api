63ab71e13053934e77529f760545a37080c618b4
odootest2023 <odootest2023@163.com>
2025-02-13 15:40:32 +0800

    refine generate-nginx-conf.js

--- generate-nginx-conf.js
+++ generate-nginx-conf.js
@@ -3,33 +3,6 @@ const fs = require('fs')
 
 const dirs = fs.readdirSync('./app')
 
-const apps = dirs.map((dir, i) => {
-  if (dir.startsWith('node')) {
-    return {
-      name   : dir,
-      script : `./app/${dir}/app.js`,
-      env: {
-        PORT: 4000 + i,
-      },
-    }
-  } else if (dir.startsWith('next')) {
-    return {
-      name   : dir,
-      script : `./app/${dir}/node_modules/next/dist/bin/next`,
-      args: `start ./app/${dir} -p ${4000 + i}`,
-    }
-  } else if (dir.startsWith('python')) {
-    return {
-      name   : dir,
-      interpreter: `./app/${dir}/env/bin/python3`,
-      script : `./app/${dir}/app.py`,
-      env: {
-        PORT: 4000 + i,
-      },
-    }
-  }
-}).filter(d => d)
-
 const upstream = dirs.map((dir, i) => `
 upstream ${dir} {
   server 127.0.0.1:${4000 + i};
