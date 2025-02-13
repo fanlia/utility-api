65c0cec619743259f82943ce88be5b9cf49d4346
odootest2023 <odootest2023@163.com>
2025-02-13 22:04:19 +0800

    refine install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,23 +6,27 @@ for f in $FILES
 do
   echo $f
   cd $f
-  if [[ $f = node* ]]
+  if [[ -f app.js ]]
   then
     echo `pwd`
+    echo node
     npm i
-  elif [[ $f = next* ]]
+  elif [[ -f next.config.ts ]]
   then
     echo `pwd`
-    npm i
-    npm run build
-  elif [[ $f = nuxt* ]]
-  then
-    echo `pwd`
+    echo next
     npm i
     npm run build
-  elif [[ $f = python* ]]
+  elif [[ -f nuxt.config.ts ]]
   then
     echo `pwd`
+    echo nuxt
+    npm i
+    npm run build
+  elif [[ -f app.py ]]
+  then
+    echo `pwd`
+    echo python
     python3 -m venv env
     source env/bin/activate && pip install -r requirements.txt
   fi
